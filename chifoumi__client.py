f738ee3119c3b36861efe1ce7c82544ca488b31a
ACER-SyméonRB <dev.chresyr@gmail.com>
2022-07-21 23:17:51 +0200

    Updated for baopig 0.19.0

--- chifoumi__client.py
+++ chifoumi__client.py
@@ -1,103 +1,20 @@
-import socket
 
+
+# import sys
+# sys.path.insert(0, 'C:\\Users\\symrb\\Documents\\python\\baopig')
 import baopig as bp
-import pygame
 
-from networking import Player, Game, Network, console_debug
-
-
-class ChifoumiPlayer(Player):
-
-    def __init__(self, ingame_id):
-
-        self.ingame_id = ingame_id  # Can change from a game to another
-        self.choice = None
-
-    chose = property(lambda self: self.choice is not None)
+import socket
+from networking import Network, console_debug
 
 
-class ChifoumiGame(Game):
+class ChifumiScene(bp.Scene):
 
-    MAX_PLAYERS_AMOUNT = 2
+    def __init__(self, *args, **kwargs):
 
-    def __init__(self, id):
+        bp.Scene.__init__(self, *args, **kwargs)
 
-        Game.__init__(self, id)
 
-        self.wins = [0, 0]
-        self.ties = 0
-        self.p1 = None
-        self.p2 = None
-
-        def newgame():
-            self._add_news("newgame")
-            self.reset_chose()
-        self.newgame_timer = bp.Timer(3, newgame)
-
-    def action(self, data, player_id):
-
-        if data == "get_news":
-            return self._send_news(player_id)
-        if data == "get_game":
-            raise PermissionError
-        else:
-            self.play(player_id, data)
-
-    def both_chose(self):
-
-        return self._players[0].chose and self._players[1].chose
-
-    def handle_close(self):
-
-        self.newgame_timer.cancel()
-
-    def get_player(self, player_id):
-
-        return self._players[player_id]
-
-    def play(self, player_id, move):
-
-        assert move in ("PIERRE", "PAPIER", "CISEAUX")
-        self._players[player_id].choice = move
-        self._add_news(f"choice:{player_id}:{move}")
-
-        if self.both_chose():
-            self._add_news(f"winner:{self.get_winner()}")
-            self.newgame_timer.start()
-
-    def rem_player(self, player):
-
-        super().rem_player(player)
-        self._want_to_be_closed = True
-
-    def reset_chose(self):
-
-        for player in self._players.values():
-            player.choice = None
-
-    def get_winner(self):
-
-        p1 = self._players[0].choice[-1]
-        p2 = self._players[1].choice[-1]
-
-        if p1 == p2:  # Tie
-            return -1
-
-        winner = -1
-        if p1 == "E" and p2 == "X":
-            winner = 0
-        elif p1 == "X" and p2 == "E":
-            winner = 1
-        elif p1 == "R" and p2 == "E":
-            winner = 0
-        elif p1 == "E" and p2 == "R":
-            winner = 1
-        elif p1 == "X" and p2 == "R":
-            winner = 0
-        elif p1 == "R" and p2 == "X":
-            winner = 1
-
-        return winner
 
 
 class ChifoumiApp(bp.Application):
@@ -107,20 +24,21 @@ class ChifoumiApp(bp.Application):
         bp.Application.__init__(self, theme="dark", size=(700, 700))
         self.set_style_for(bp.Text, font_height=40)
 
-        # MENU
+        # SCENES
         menu_scene = bp.Scene(self, name="menu")
-        bp.Button(menu_scene, "JOUER !", sticky="center", size=(700, 200),
-                  command=bp.PrefilledFunction(self.open, "waiting"))
+        wait_scene = WaitScene(self, name="waiting")
+        self.play_scene = PlayScene(self, "playground")
+
+        # MENU
+        bp.Button(menu_scene, "JOUER !", sticky="center", size=(700, 200), command=wait_scene.open)
         self.ipaddr = "127.0.0.1"
-        servaddr_title = bp.Text(menu_scene, "adresse IP du serveur :", pos=("50%", "10%"), pos_location="top")
-        bp.Entry(menu_scene, text=self.ipaddr, width=280, entry_type=str,
-                 pos_location="top", pos_ref=servaddr_title, pos_ref_location="bottom",
-                 command=lambda text: setattr(self, "ipaddr", text))
+        servaddr_title = bp.Text(menu_scene, "adresse IP du serveur :", midtop=("50%", "10%"))
+        bp.Entry(menu_scene, text=self.ipaddr, width=280, loc="midtop", ref=servaddr_title, refloc="midbottom",
+                 entry_type=str, command=lambda text: setattr(self, "ipaddr", text), padding=5)
 
         # WAITING
-        wait_scene = WaitScene(self, name="waiting")
         search = bp.Text(wait_scene, "A la recherche\nd'un adversaire", sticky="center", align_mode="center")
-        self.search_animayion = bp.Text(wait_scene, "", pos_ref=search, pos_location="top", pos_ref_location="bottom")
+        self.search_animayion = bp.Text(wait_scene, "", ref=search, loc="midtop", refloc="midbottom")
         self.search_animation_index = 0
         def animate_serach():
             self.search_animation_index = (self.search_animation_index + 1) % 4
@@ -128,25 +46,27 @@ class ChifoumiApp(bp.Application):
         self.search_animator = bp.RepeatingTimer(.4, animate_serach)
 
         # PLAYGROUND
-        self.play_scene = PlayScene(self, "playground")
         def menu():
             self.play_scene.network.disconnect()
-            self.open("menu")
+            menu_scene.open()
         bp.Button(wait_scene, "MENU", command=menu)
         bp.Button(self.play_scene, "MENU", command=menu)
 
         # DIALOGS
         self.set_style_for(bp.DialogFrame, width="80%")
-        self.end_dialog = bp.Dialog(self, "Votre adversaire a quitté\nla partie en cours",
-                                    choices=("MENU", "NOUVELLE PARTIE"), name="end_dialog")
+        self.end_dialog = bp.Dialog(self, title="Arrêt pématuré", name="end_dialog",
+                                    description="Votre adversaire a quitté\nla partie en cours",
+                                    choices=("MENU", "NOUVELLE PARTIE"))
         def click_end_dialog(choice):
             if choice == "MENU":
                 menu_scene.open()
             else:
                 wait_scene.open()
-        self.end_dialog.signal.ANSWERED.connect(click_end_dialog)
+        self.end_dialog.signal.ANSWERED.connect(click_end_dialog, owner=None)
 
-    def close(self):
+    def _close(self):
+
+        super()._close()
 
         if self.play_scene.network and self.play_scene.network.is_connected:
             self.play_scene.network.disconnect()
@@ -154,58 +74,62 @@ class ChifoumiApp(bp.Application):
 
 class WaitScene(bp.Scene):
 
-    def close(self):
+    def _close(self):
 
-        self.app.search_animator.cancel()
+        super()._close()
+
+        self.application.search_animator.cancel()
 
     def open(self):
 
-        play = self.app.play_scene
+        super().open()
+
+        play = self.application.play_scene
 
         try:
-            play.network = Network(ip_addr=self.app.ipaddr, port=5554)
+            play.network = Network(ip_addr=self.application.ipaddr, port=5554)
         except socket.error:
-            bp.LOGGER.warning(f"No server found at address {self.app.ipaddr}")
-            return self.app.open("menu")
+            bp.LOGGER.warning(f"No server found at address {self.application.ipaddr}")
+            return self.application.open("menu")
 
         play.client_id = play.network.get_client_id()
         play.you_are_player.set_text(f"Vous êtes le joueur n°{play.client_id}")
-        self.app.search_animator.start()
+        self.application.search_animator.start()
 
     def run(self):
 
-        if self.app.play_scene.network.send("/game_started") == "True":
-            self.app.play_scene.open()
+        if self.application.play_scene.network.send("/game_started") == "True":
+            self.application.play_scene.open()
 
 
 class PlayScene(bp.Scene):
 
-    def __init__(self, app, name):
+    def __init__(self, application, name):
 
-        bp.Scene.__init__(self, app, name=name)
+        bp.Scene.__init__(self, application, name=name)
 
         self.network = None
         self.client_id = None
         self.game = None
 
-        self.you_are_player = bp.Text(self, "", sticky="top")
+        self.you_are_player = bp.Text(self, "", sticky="midtop")
         border_width = 5
-        line = bp.Line(self, (0, 0, 0), (0, self.you_are_player.bottom + border_width),
-                       (self.w, self.you_are_player.bottom + border_width), width=border_width)
+        line = bp.Line(self, (0, 0, 0), (0, self.you_are_player.rect.bottom + border_width),
+                       (self.rect.w, self.you_are_player.rect.bottom + border_width), width=border_width)
         self.result = bp.Text(self, "", sticky="center", font_height=90, visible=False)
-        other_choice_title = bp.Text(self, "Votre adversaire a choisi :", pos=(0, border_width),
-                                     pos_ref=line, pos_location="top", pos_ref_location="bottom")
-        self.other_choice_text = bp.Text(self, "-",
-                                         pos_ref=other_choice_title, pos_location="top", pos_ref_location="bottom")
+        other_choice_title = bp.Text(self, "Votre adversaire a choisi :", midtop=(0, border_width),
+                                     ref=line, refloc="midbottom")
+        self.other_choice_text = bp.Text(self, "-", ref=other_choice_title, loc="midtop", refloc="midbottom")
 
-        btns_zone = bp.Zone(self, width="100%", height="30%", sticky="bottom")
+        btns_zone = bp.Zone(self, width="100%", height="30%", sticky="midbottom")
         btns_zone.set_style_for(bp.Button, width="30%", height="90%")
 
         self.chose = False
         self.other_choice = None
-        self.choice_text = bp.Text(self, "", pos=btns_zone.midtop, pos_location="bottom")
+        self.choice_text = bp.Text(self, "", midbottom=btns_zone.rect.midtop)
         class ChoiceButton(bp.Button):
-            def validate(btn, *args, **kwargs):
+            def handle_validate(btn):
+                super().handle_validate()
                 if self.chose:
                     return
                 self.chose = True
@@ -216,7 +140,9 @@ class PlayScene(bp.Scene):
         self.paper = ChoiceButton(btns_zone, "PAPIER", sticky="center", name="Paper")
         self.scissors = ChoiceButton(btns_zone, "CISEAUX", sticky="midright", name="Scissors")
 
-    def close(self):
+    def _close(self):
+
+        super()._close()
 
         self.chose = False
         self.other_choice = None
@@ -226,13 +152,14 @@ class PlayScene(bp.Scene):
 
     def run(self):
 
-        if console_debug: print("RUN")
+        if console_debug:
+            print("RUN")
 
         try:
             all_news = self.network.send("get_news")
             if console_debug: print(f"RECEIVED NEWS : -{all_news}-")
             if all_news is None:
-                return self.app.end_dialog.open()
+                return self.application.end_dialog.open()
             if all_news != "n":
                 for news in all_news.split("|"):
                     if not news:  # always a first empty string
@@ -267,7 +194,7 @@ class PlayScene(bp.Scene):
             bp.LOGGER.warning(e)
             if self.network.is_connected:
                 self.network.disconnect()
-            self.app.end_dialog.open()
+            self.application.end_dialog.open()
 
 
 if __name__ == '__main__':
--- chifoumi__server.py
+++ chifoumi__server.py
@@ -1,7 +1,104 @@
 
 
-from networking import Server
-from chifoumi__client import ChifoumiGame, ChifoumiPlayer
+# import sys
+# sys.path.insert(0, 'C:\\Users\\symrb\\Documents\\python\\baopig')
+import baopig as bp
+from networking import Server, Player, Game
 
-server = Server(port=5554, game_class=ChifoumiGame, player_class=ChifoumiPlayer, max_connections=2)
+
+class ChifoumiPlayer(Player):
+
+    def __init__(self, ingame_id):
+
+        self.ingame_id = ingame_id  # Can change from a game to another
+        self.choice = None
+
+    chose = property(lambda self: self.choice is not None)
+
+
+class ChifoumiGame(Game):
+
+    MAX_PLAYERS_AMOUNT = 2
+
+    def __init__(self, id):
+
+        Game.__init__(self, id)
+
+        self.wins = [0, 0]
+        self.ties = 0
+        self.p1 = None
+        self.p2 = None
+
+        def newgame():
+            self._add_news("newgame")
+            self.reset_chose()
+        self.newgame_timer = bp.Timer(3, newgame)
+
+    def action(self, data, player_id):
+
+        if data == "get_news":
+            return self._send_news(player_id)
+        if data == "get_game":
+            raise PermissionError
+        else:
+            self.play(player_id, data)
+
+    def both_chose(self):
+
+        return self._players[0].chose and self._players[1].chose
+
+    def handle_close(self):
+
+        self.newgame_timer.cancel()
+
+    def get_player(self, player_id):
+
+        return self._players[player_id]
+
+    def play(self, player_id, move):
+
+        assert move in ("PIERRE", "PAPIER", "CISEAUX")
+        self._players[player_id].choice = move
+        self._add_news(f"choice:{player_id}:{move}")
+
+        if self.both_chose():
+            self._add_news(f"winner:{self.get_winner()}")
+            self.newgame_timer.start()
+
+    def rem_player(self, player):
+
+        super().rem_player(player)
+        self._want_to_be_closed = True
+
+    def reset_chose(self):
+
+        for player in self._players.values():
+            player.choice = None
+
+    def get_winner(self):
+
+        p1 = self._players[0].choice[-1]
+        p2 = self._players[1].choice[-1]
+
+        if p1 == p2:  # Tie
+            return -1
+
+        winner = -1
+        if p1 == "E" and p2 == "X":
+            winner = 0
+        elif p1 == "X" and p2 == "E":
+            winner = 1
+        elif p1 == "R" and p2 == "E":
+            winner = 0
+        elif p1 == "E" and p2 == "R":
+            winner = 1
+        elif p1 == "X" and p2 == "R":
+            winner = 0
+        elif p1 == "R" and p2 == "X":
+            winner = 1
+
+        return winner
+
+
+server = Server(port=5544, game_class=ChifoumiGame, player_class=ChifoumiPlayer, max_connections=2)
 server.start_listening()
